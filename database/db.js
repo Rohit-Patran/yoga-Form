c8c8dce49352a81223310b09db09e16a22127398
Rohit Patran <90060873+Rohit-Patran@users.noreply.github.com>
2022-12-16 03:15:23 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -5,10 +5,10 @@ dotenv.config();
 
 const dbURL = process.env.mongoDB_URL;
 //database connnection
-const connection = function()
+const connection = async function()
 {
     mongoose.set('strictQuery' , false);
-    mongoose.connect(dbURL).then(() => {
+    await mongoose.connect(dbURL).then(() => {
         console.log("database successfully connected");
     }).catch(()=>{
         console.log("error in database connection")
