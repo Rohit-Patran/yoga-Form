2aa01f5743639a5300cfc62b56bc8a1de2f6976f
Rohit Patran <90060873+Rohit-Patran@users.noreply.github.com>
2022-12-16 13:17:32 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -7,7 +7,7 @@ dotenv.config();
 const connection = async function()
 {
     mongoose.set('strictQuery' , false);
-    await mongoose.connect(process.env.MONGO_URI).then(() => {
+    await mongoose.connect("mongodb+srv://rohit:Rpmongodbatalas799@cloud-database.f39byvj.mongodb.net/?retryWrites=true&w=majority").then(() => {
         console.log("database successfully connected");
     }).catch((error)=>{
         console.log("error in database connection:" , error);
